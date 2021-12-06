0e5612f9d2e18492279844a49ba8c36ab0a23332
Akash <95037464+AkashsRepositories@users.noreply.github.com>
2021-12-06 23:35:29 +0530

    Add files via upload

--- Recursion2.java
+++ Recursion2.java
@@ -24,28 +24,54 @@ public class Recursion2 {
     // }
 
 
-    public static int first = -1;
-    public static int last  = -1;
+    // public static int first = -1;
+    // public static int last  = -1;
 
-    public static void findOccurance(String str, int idx, char element) {
-        if(idx == str.length()) {
-            System.out.println(first);
-            System.out.println(last);
-            return;
+    // public static void findOccurance(String str, int idx, char element) {
+    //     if(idx == str.length()) {
+    //         System.out.println(first);
+    //         System.out.println(last);
+    //         return;
+    //     }
+
+    //     char currChar = str.charAt(idx);
+    //     if(currChar == element){
+    //         if(first == -1){
+    //             first = idx;
+    //             last = idx;
+    //         } else {
+    //             last = idx;
+    //         }
+    //     }
+
+    //     findOccurance(str, idx + 1, element);
+    // }
+
+
+
+    public static boolean isSorted(int arr[], int idx)  {
+        
+        //arr.length-1 because we are using idx+1 in next if statement
+        if(idx == arr.length - 1) {
+            return true;
         }
 
-        char currChar = str.charAt(idx);
-        if(currChar == element){
-            if(first == -1){
-                first = idx;
-                last = idx;
-            } else {
-                last = idx;
-            }
-        }
+        // if(arr[idx] < arr[idx + 1]){
+        //     //array is strictly sorted until now then do a recursive call to check remaining elements 
+        //     return isSorted(arr, idx + 1);        
+        // } else{
+        //     return false;
+        // }
 
-        findOccurance(str, idx + 1, element);
+        if(arr[idx] >= arr[idx + 1]){
+            return false;
+        } 
+            
+        return isSorted(arr, idx+1);
+        
     }
+
+
 
 
     public static void main(String[] args) {
@@ -58,7 +84,13 @@ public class Recursion2 {
 
 
         
-        String str = "abaacdacfaah";
-        findOccurance(str, 0, 'a');
+        // String str = "abaacdacfaah";
+        // findOccurance(str, 0, 'a');
+
+
+        int arr[] = { -1, 0, 2, 5};
+        boolean result = isSorted(arr, 0);
+        System.out.println(result);
+
     }
 }
